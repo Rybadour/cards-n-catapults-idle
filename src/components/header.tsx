2248407989d7395297fc8aa9e49e804e03cf11c4
Ryan Badour <rybadour@gmail.com>
2022-07-07 18:47:27 -0400

    Added basic help modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,23 @@
 import './header.scss';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 function Header() {
+  const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
+
   return <header>
     <h1>Cards & Catapults Idle</h1>
 
     <div className="options">
-      <button>Save</button>
-      <button>Load</button>
+      <button onClick={() => setIsHelpModalOpen(true)}>Help</button>
       <button>Options</button>
     </div>
+
+    <Modal
+      isOpen={isHelpModalOpen}
+    >
+      <h2>Help Tips</h2>
+    </Modal>
   </header>;
 }
 
