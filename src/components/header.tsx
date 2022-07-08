636565c88577b0a2e62cadab6a546372d8501ed9
rybadour <rybadour@gmail.com>
2022-07-08 09:26:56 -0400

    Added help tips and menu to select them

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,15 @@
 import './header.scss';
 import Modal from 'react-modal';
 import { useState } from 'react';
+import classNames from 'classnames';
+
+const modalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+  },
+};
+
+Modal.setAppElement('#root');
 
 function Header() {
   const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
@@ -15,10 +24,101 @@ function Header() {
 
     <Modal
       isOpen={isHelpModalOpen}
+      onRequestClose={() => setIsHelpModalOpen(false)}
+      style={modalStyles}
+      contentLabel="Help Tips"
+      className="help-modal-content"
     >
-      <h2>Help Tips</h2>
+      <HelpModal />
     </Modal>
   </header>;
+}
+
+function HelpModal() {
+  const [selected, setSelected] = useState('controls');
+
+  const helpTips = [{
+    id: 'controls',
+    title: 'Controls',
+  }, {
+    id: 'food-drain',
+    title: 'Food Drain',
+  }, {
+    id: 'expired-food',
+    title: 'Expired Food',
+  }, {
+    id: 'produce-cards',
+    title: 'Card Production',
+  }, {
+    id: 'bonuses',
+    title: 'Bonuses',
+  }]
+
+  return <div>
+    <h2>Help Tips</h2>
+
+    <div className="help-body-wrapper">
+      <ul className="help-tip-menu">
+        {helpTips.map(tip => 
+          <li
+            className={classNames({selected: tip.id == selected})}
+            onClick={() => setSelected(tip.id)}
+          >{tip.title}</li>
+        )}
+      </ul>
+      <div className="help-body">
+        <div className={classNames({selected: selected == 'controls'})}>
+          <h3>Controls</h3>
+          <p>To place a card select it by clicking on it then <b>left clicking on the grid</b>. If a card is already in that
+          space it will <b>replace it</b> and the existing card will <b>go back to your inventory</b>.</p>
+          <p>To remove a card from the grid you <b>right click it</b>.</p>
+          <p>When removing a food card it will <b>retain its remaining capacity</b> in the form of a decimal number. Ex. If
+          you remove a food card at half capacity it will be returned to your inventory as 0.5 of a card. When placing
+          a food card you can place that fraction of a card and it will place it at the remaining capacity.</p>
+        </div>
+
+        <div className={classNames({selected: selected == 'food-drain'})}>
+          <h3>Food Drain</h3>
+          <p>You’ll find that most person cards (Beggars and Peasants) and some special cards will have a food drain stat.
+          This is the rate at which they eat food and it’s always listed as per second. Cards generally eat food that is
+          adjacent to them (orthogonally) and will eat food from all those cards at an equal rate.</p>
+          <p>Ex. In these screenshots we have a Beggar next to one food card and another Beggar surrounded by food cards.</p>
+          <p>A Beggar eats food at 0.2 units per second so in the first screenshot it will eat that food card at 0.2
+          units per second. In the second since it’s surrounded by 4 food cards each will be drained at ¼ the speed or
+          0.05 units per second.</p>
+        </div>
+
+        <div className={classNames({selected: selected == 'expired-food'})}>
+          <h3>Expired Food</h3>
+
+          <p>You may notice that when food is completely used up it remains as faded out. In this state the card is
+          completely disabled but it’s used as an indicator that it can be automatically replaced. Some cards will
+          automatically replace food cards and they will only do this based on these expired food tiles. Cards that
+          generate cards adjacent to them can override these expired food tiles and place different cards there
+          automatically.</p>
+        </div>
+
+        <div className={classNames({selected: selected == 'produce-cards'})}>
+          <h3>Cards that Produce Cards</h3>
+
+          <p>Some cards will constantly produce cards for free. Generally they place these cards in adjacent tiles
+            (orthogonally) but sometimes they send them straight to your inventory such as with the Pig Pen. If all
+            adjacent tiles are occupied the produced card will go into the inventory.
+          </p>
+        </div>
+
+        <div className={classNames({selected: selected == 'bonuses'})}>
+          <h3>Bonuses</h3>
+
+          <p>Some cards mention "improving" other cards. This means to increase their production by that amount or to
+          speed up their abilities by that amount. Production bonuses are applied multiplicatively. You can tell if a
+          card is apply a bonus to another card by hovering over the bonus applying card. Cards that it's applying a
+          bonus to will be highlighted green.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>;
 }
 
 export default Header;
